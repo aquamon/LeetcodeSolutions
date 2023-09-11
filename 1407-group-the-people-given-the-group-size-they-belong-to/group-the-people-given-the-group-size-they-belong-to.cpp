4a0b68d7dfb5affdd360653c4b093d8c16e3138d
MONASH CHHETRI <monash.chhetri22@gmail.com>
2023-09-11 12:43:08 +0530

    Time: 7 ms (87.01%) | Memory: 12.5 MB (96.45%) - LeetSync

--- 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.cpp
@@ -3,42 +3,50 @@ public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
         
         unordered_map<int,vector<int>>M;
-
+        vector<vector<int>>ans;
         for(int i=0;i<groupSizes.size();i++)
         {
             M[groupSizes[i]].push_back(i);
-        }
 
-        vector<vector<int>>ans;
-
-        for(auto it:M)
-        {
-            int X = it.first;
-            int sz = it.second.size();
-            vector<int> A = it.second;
-            if(sz <= X)
-                ans.push_back(it.second);
-            else
+            if(M[groupSizes[i]].size() == groupSizes[i])
             {
-                vector<int>temp;
-                int curr_size = 0;
-                for(int i=0;i<sz;i++)
-                {
-                    if(curr_size++ < X)
-                        temp.push_back(A[i]);
-                    else
-                    {
-                        ans.push_back(temp);
-                        temp.clear();
-                        temp.push_back(A[i]);
-                        curr_size = 1;
-                    }
-                }
-                if(curr_size >= 1)
-                    ans.push_back(temp);
-                
+                ans.push_back(M[groupSizes[i]]);
+                M[groupSizes[i]].clear();
             }
+                
+
         }
+
+       
+
+        // for(auto it:M)
+        // {
+        //     int X = it.first;
+        //     int sz = it.second.size();
+        //     vector<int> A = it.second;
+        //     if(sz <= X)
+        //         ans.push_back(it.second);
+        //     else
+        //     {
+        //         vector<int>temp;
+        //         int curr_size = 0;
+        //         for(int i=0;i<sz;i++)
+        //         {
+        //             if(curr_size++ < X)
+        //                 temp.push_back(A[i]);
+        //             else
+        //             {
+        //                 ans.push_back(temp);
+        //                 temp.clear();
+        //                 temp.push_back(A[i]);
+        //                 curr_size = 1;
+        //             }
+        //         }
+        //         if(curr_size >= 1)
+        //             ans.push_back(temp);
+                
+        //     }
+        // }
         
 
         return ans;
