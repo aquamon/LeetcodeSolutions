7ed18337b4eb715e5ed6c858e2549e0642a780fb
MONASH CHHETRI <monash.chhetri22@gmail.com>
2023-10-31 21:21:13 +0530

    Time: 71 ms (97.31%) | Memory: 76.3 MB (69.22%) - LeetSync

--- 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.cpp
+++ 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     vector<int> findArray(vector<int>& pref) {
     
-        vector<int>ans;
-        ans.push_back(pref[0]);
-       for(int i=1;i<pref.size();i++)
+        // vector<int>ans;
+        // ans.push_back(pref[0]);
+       for(int i=pref.size()-1;i > 0;i--)
        {
-           ans.push_back( pref[i]^pref[i-1]);
+           pref[i] = pref[i]^pref[i-1];
        } 
 
-       return ans;
+       return pref;
     }
 };
