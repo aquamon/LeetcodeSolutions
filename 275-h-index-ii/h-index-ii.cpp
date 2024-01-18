d2533559cc94b702d2b3bd4b2de059066780c89f
MONASH CHHETRI <monash.chhetri22@gmail.com>
2024-01-18 21:58:54 +0530

    Time: 3 ms (99.86%) | Memory: 19.1 MB (18.80%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+    int solve(vector<int>&A,int N)
+    {
+        int ans = 0;
+        for(int i=0;i<N;i++)
+        {
+            if(A[i] >= N-i)
+            {
+                ans = max(ans,N-i);
+            }
+        }
+        return ans;
+    }
+    
+    int hIndex(vector<int>& citations) {
+        int N = citations.size();
+        return solve(citations,N);
+        // int start = 0;
+        // int end = N-1;
+        // int ans = 0;
+        // while(start <= end)
+        // {
+        //     int mid = start + (end-start)/2;
+            
+        //     if(citations[mid] >= N-mid)
+        //     {
+        //         ans = max(ans,N-mid);
+        //         end = mid-1;
+        //     }
+        //     else
+        //         start = mid + 1;
+        // }
+        // return ans;
+    }
+};
