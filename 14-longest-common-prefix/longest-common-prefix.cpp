3189396f1a5a321373b12be5e8a1c4b07c313e30
MONASH CHHETRI <monash.chhetri22@gmail.com>
2024-01-20 22:56:02 +0530

    Time: 7 ms (19.79%) | Memory: 10.9 MB (7.42%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,71 +1,21 @@
-struct Trie{
-        bool isEnd;
-        Trie* child[26];
-        Trie(){
-            isEnd = false;
-            for(int i=0;i<26;i++)
-                child[i] = NULL;
-        }
-
-        bool containsChar(char ch){
-            return (child[ch-'a'] != NULL);
-        }
-
-        void put(char ch,Trie* node){
-            child[ch-'a'] = node;
-        }
-
-        Trie* get(char ch){
-            return child[ch-'a'];
-        }
-    };
 class Solution {
 public:
-
-    int countChild(Trie* node, int *index){
-        int count = 0;
-        for(int i=0;i<26;i++){
-            if(node->child[i] != NULL){
-                count++;
-                *index = i;
-            }
-        }
-
-        return count;
-    }
-    string traverse(Trie* root){
-        int index=0;
-        string prefix;
-
-        Trie* temp = root;
-
-        while(countChild(temp,&index) == 1 and temp->isEnd == false){
-            prefix.push_back('a'+index);
-            temp = temp->child[index];
-        }
-
-        return prefix;
-    }
     string longestCommonPrefix(vector<string>& strs) {
-        
-        Trie* root = new Trie();
+        sort(strs.begin(),strs.end());
 
-        for(string S : strs){
-            if(S=="")
-                return "";
-            
-            Trie* temp = root;
-            for(int i=0;i<S.size();i++){
-                if(!temp->containsChar(S[i])){
-                    temp->put(S[i],new Trie());
-                }
-                temp = temp->get(S[i]);
+        string a = strs[0];
+        string b = strs[strs.size()-1];
+
+        string ans = "";
+        for(int i=0;i<min(a.size(),b.size());i++)
+        {
+            if(a[i] == b[i])
+            {
+                ans += a[i];
             }
-            temp->isEnd = true;
+            else
+                break;
         }
-
-        string prefix = traverse(root);
-
-        return prefix;
+        return ans;
     }
 };
