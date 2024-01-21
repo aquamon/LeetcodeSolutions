5d094c1f962c8da36daecdb074e544790216fbd7
MONASH CHHETRI <monash.chhetri22@gmail.com>
2024-01-21 10:22:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (5.15%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        
+        int j=0;
+        for(int i=0;i<t.length();i++)
+        {
+            if(t[i] == s[j])
+            {
+                j++;
+            }
+        }
+        return j == s.length();
+    }
+};
