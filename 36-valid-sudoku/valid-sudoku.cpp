89e8f7044312b368ff2b7caf5a12f1f50c0ef82c
MONASH CHHETRI <monash.chhetri22@gmail.com>
2024-01-24 16:09:31 +0530

    Time: 20 ms (47.62%) | Memory: 25.7 MB (5.01%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        
+        vector<unordered_map<int,int>>ROW(9);
+        vector<unordered_map<int,int>>COL(9);
+        vector<unordered_map<int,int>>BOX(9);
+
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+                if(board[i][j] == '.')
+                    continue;
+                int curr = board[i][j] - '0';
+                cout<<curr<<" ";
+                if(ROW[i].find(curr) != ROW[i].end())
+                    return false;
+                if(COL[j].find(curr) != COL[i].end())
+                    return false;
+                int box = (i/3) + (j/3) + ((i/3)*2);
+                if(BOX[box].find(curr) != BOX[box].end())
+                    return false;
+                
+                ROW[i][curr]++;
+                COL[j][curr]++;
+                BOX[box][curr]++;
+
+
+            }
+        }
+        return true;
+    }
+};
