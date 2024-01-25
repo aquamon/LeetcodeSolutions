607d9fe9d39500535afa0bb12cc4bc1ad0bf763b
MONASH CHHETRI <monash.chhetri22@gmail.com>
2024-01-25 20:46:36 +0530

    Time: 3 ms (99.68%) | Memory: 16.1 MB (6.07%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,28 +1,75 @@
 class Solution {
 public:
+    void makeZeroRow(int row,vector<vector<int>>&matrix)
+    {
+        for(int i=0;i<matrix[0].size();i++)
+            matrix[row][i] = 0;
+    }
+     void makeZeroCol(int col,vector<vector<int>>&matrix)
+    {
+        for(int i=0;i<matrix.size();i++)
+            matrix[i][col] = 0;
+    }
     void setZeroes(vector<vector<int>>& matrix) {
         
-        unordered_map<int,int>row,col;
+        
+        bool firstRowZero = false;
+        bool firstColZero = false;
 
-        for(int i=0;i<matrix.size();i++)
+        for(int j=0;j<matrix[0].size();j++)
         {
-            for(int j=0;j<matrix[0].size();j++)
+            if(matrix[0][j] == 0)
+            {
+                firstRowZero = true;
+                break;
+            }
+        }
+        for(int j=0;j<matrix.size();j++)
+        {
+            if(matrix[j][0] == 0)
+            {
+                firstColZero = true;
+                break;
+            }
+        }
+
+        for(int i=1;i<matrix.size();i++)
+        {
+            for(int j=1;j<matrix[0].size();j++)
             {
                 if(matrix[i][j] == 0)
                 {
-                    row[i]++;
-                    col[j]++;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-
-        for(int i=0;i<matrix.size();i++)
+        for(int j=1;j<matrix[0].size();j++)
         {
-            for(int j=0;j<matrix[0].size();j++)
+            if(matrix[0][j] == 0)
             {
-                if(row.find(i) != row.end() or col.find(j) != col.end())
-                    matrix[i][j] = 0;
+                makeZeroCol(j,matrix);
             }
         }
+        for(int i=1;i<matrix.size();i++)
+        {
+            if(matrix[i][0] == 0)
+            {
+                makeZeroRow(i,matrix);
+
+            }
+        }
+
+        if(firstRowZero)
+            fill(matrix[0].begin(),matrix[0].end(),0);
+        
+        if(firstColZero)
+        {
+            for(int i=0;i<matrix.size();i++)
+                matrix[i][0] = 0;
+        }
+       
+
+
     }
 };
