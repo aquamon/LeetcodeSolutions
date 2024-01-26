fdd3abc81a6958880b82c4194eb1a0dfa69be214
MONASH CHHETRI <monash.chhetri22@gmail.com>
2024-01-26 22:25:34 +0530

    Time: 154 ms (52.37%) | Memory: 74 MB (14.83%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,38 +1,28 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if(nums.size() < 1)
-            return 0;
-        if(nums.size() == 1)
-            return 1;
-        unordered_map<int,int>M,vis;
-
-        for(int X : nums)
-        {
-            M[X]++;
-        }
-
+        
+        unordered_map<int,int>M;
         int ans = 0;
         for(int i=0;i<nums.size();i++)
         {
-            if(M.find(nums[i]-1) != M.end())
-                continue;
-            else
-            {
-                if(vis.find(nums[i]) != vis.end())
-                    continue;
-                vis[nums[i]] = 1;
-
-                int x = nums[i], len = 0;
-
-                while(M.find(x) != M.end())
+            if(M.find(nums[i]) != M.end())
+                continue;     
+            int left_streak = 0, right_streak = 0;
+                if(M.find(nums[i]-1) != M.end())
                 {
-                    len++;
-                    x++;
+                    left_streak = M[nums[i]-1];
                 }
-                ans = max(ans,len);
-                
-            }
+                if(M.find(nums[i]+1) != M.end())
+                {
+                    right_streak = M[nums[i]+1];
+                }
+
+                ans = max(ans,1+left_streak+right_streak);
+
+                M[nums[i]] = 1+left_streak+right_streak;
+                M[nums[i]-left_streak] = 1+left_streak+right_streak;
+                M[nums[i]+right_streak] = 1+left_streak+right_streak;   
         }
         return ans;
     }
