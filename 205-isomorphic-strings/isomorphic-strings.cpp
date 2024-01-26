3489cd60314b931fda8e768360b7f5352115c874
MONASH CHHETRI <monash.chhetri22@gmail.com>
2024-01-26 14:20:52 +0530

    Time: 4 ms (76.73%) | Memory: 9.4 MB (8.22%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -4,18 +4,30 @@ public:
         
         if(s.size() != t.size())
             return false;
-        int freqS[256] = {0};
-        int freqT[256] = {0};
+       unordered_map<char,char>M1;
+       unordered_map<char,char>M2;
+       
 
-        for(int i=0;i<s.size();i++)
-        {
-            
-            if(freqT[t[i]] != freqS[s[i]])
+       for(int i=0;i<t.size();i++)
+       {
+           if(M1.find(s[i]) != M1.end())
+           {
+               if(M1[s[i]] != t[i])
                 return false;
-            freqS[s[i]] = i+1;;
-            freqT[t[i]] = i+1;
-        }
-        return true;
-        
+           }
+           else
+           {
+               if(M2.find(t[i]) != M2.end())
+               {
+                   return false;
+               }
+               else
+               {
+                   M1[s[i]] = t[i];
+                   M2[t[i]] = s[i];
+               }
+           }
+       }
+       return true;
     }
 };
