7c893f2fd65350ee12d8326056b5077a674febc9
MONASH CHHETRI <monash.chhetri22@gmail.com>
2024-01-26 22:04:53 +0530

    Time: 920 ms (24.61%) | Memory: 74 MB (14.83%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        
+
+        if(nums.size() < 1)
+            return 0;
         unordered_map<int,int>M;
-        int ans = 0;
+
         for(int i=0;i<nums.size();i++)
         {
-            if(M.find(nums[i]) != M.end())
-                continue;
-            
-            int left_streak = 0, right_streak = 0;
+            M[nums[i]]++;
+        }
 
-            if(M.find(nums[i]-1) != M.end())
-                left_streak = M[nums[i]-1];
-            if(M.find(nums[i] + 1) != M.end())
-                right_streak = M[nums[i] + 1];
-            
-            ans = max(ans,1+left_streak + right_streak);
-
-            M[nums[i]] = 1 + left_streak + right_streak;
-            M[nums[i]-left_streak] = 1 + left_streak + right_streak;
-            M[nums[i]+right_streak] = 1 + left_streak + right_streak;
-
-
+        int ans = 1;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(M.find(nums[i]-1) == M.end())
+            {
+                int length = 1;
+                int curr = nums[i];
+                while(true)
+                {
+                    if(M.find(++curr) != M.end())
+                        length += 1;
+                    else
+                        break;
+                }
+                ans = max(ans,length);
+            }
         }
         return ans;
     }
