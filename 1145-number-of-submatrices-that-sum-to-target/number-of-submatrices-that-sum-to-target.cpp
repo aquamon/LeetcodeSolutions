f2274f752f5ab028088b7ff614573b8927c15588
MONASH CHHETRI <monash.chhetri22@gmail.com>
2024-01-28 16:21:25 +0530

    Time: 673 ms (50.12%) | Memory: 279.4 MB (6.14%) - LeetSync

--- 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.cpp
+++ 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.cpp
@@ -1,42 +1,9 @@
 class Solution {
 public:
-  int sum(int r1, int c1, int r2 ,int c2, int M, int N,vector<vector<int>>& matrix)
-    {
-        int A = matrix[r2][c2];
-        int B = 0, C=0, D=0;
-
-        if(r1-1 >= 0)
-            B = matrix[r1-1][c2];
-        
-        if(c1-1 >= 0)
-            C = matrix[r2][c1-1];
-        
-        if(r1-1 >= 0 and c1-1>=0)
-            D = matrix[r1-1][c1-1];
-        
-
-        return A-B-C+D;
-    }
-    int solve(int r1, int c1, vector<vector<int>>& matrix, int target,int M, int N)
-    {
-        int ans = 0;
-        for(int i=r1; i < M; i++)
-        {
-            for(int j=c1; j<N;j++)
-            {
-               
-                
-                
-                int r2 = i, c2 = j;
-
-                if(sum(r1,c1,r2,c2,M,N,matrix) == target)
-                    ans++;
-            }
-        }
-        return ans;
-    }
+  
     int numSubmatrixSumTarget(vector<vector<int>>& matrix, int k) {
-         int M = matrix.size();
+        
+        int M = matrix.size();
         int N = matrix[0].size();
         int ans = 0;
         for(int i=0;i<M;i++)
@@ -55,14 +22,33 @@ public:
             }
         }
 
-        for(int i=0;i<M;i++)
+         for(int r1=0;r1<M;r1++)
         {
-            for(int j=0;j<N;j++)
+            for(int r2 = r1;r2<M;r2++)
             {
-                int r1 = i, c1 = j;
-                ans += solve(r1,c1,matrix,k,M,N);
+                unordered_map<int,int>subMatrixSum;
+
+                subMatrixSum[0] = 1;
+
+                for(int c2 = 0;c2 < N; c2++)
+                {
+                    int up = r1-1 >= 0 ? matrix[r1-1][c2] : 0;
+                    int current_sum = matrix[r2][c2] - up;
+
+                    int other = current_sum - k;
+
+                    ans += subMatrixSum[other];
+
+                    subMatrixSum[current_sum]++;
+
+
+                }
             }
         }
+
         return ans;
+
+
+        
     }
 };
