4903ea46159f664cfbf581517b11b04cb368ff7d
MONASH CHHETRI <monash.chhetri22@gmail.com>
2024-02-04 13:27:18 +0530

    Time: 5 ms (89.63%) | Memory: 10.3 MB (15.00%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -2,60 +2,65 @@ class Solution {
 public:
     string minWindow(string s, string t) {
         
-        if(t.length()<1)
-            return "";
         int freqS[256] = {0};
         int freqT[256] = {0};
-
         int dist_char = 0;
-        for(int i=0;i<t.length();i++)
+        for(int i=0;i<t.size();i++)
         {
             if(freqT[t[i]] == 0)
+            {
                 dist_char++;
+            }
             freqT[t[i]]++;
         }
 
-       
         int count = 0;
-        int start = 0, end = 0;
-        for(int j=0;j<s.length();j++)
+        int end = 0;
+        for(int i=0;i<s.size();i++)
         {
-            freqS[s[j]]++;
-            if(freqS[s[j]] == freqT[s[j]])
+            freqS[s[i]]++;
+            if(freqS[s[i]] == freqT[s[i]])
+            {
                 count++;
-            
+            }
             if(count == dist_char)
             {
-                end = j;
+                end = i;
                 break;
             }
         }
 
         if(count < dist_char)
             return "";
-        
+        int start = 0;
+        int ans_start = 0, ans_end = end;
         int len = end-start+1;
-        int ans_s = start, ans_e = end;
+    
 
-        while(end < s.length())
+        while(end < s.size())
         {
             while(freqS[s[start]] > freqT[s[start]])
             {
                 freqS[s[start]]--;
                 start++;
+                
             }
 
-            if((end-start+1) < len)
+            if(end-start+1 < len)
             {
+                ans_start = start;
+                ans_end = end;
                 len = end-start+1;
-                ans_s = start;
-                ans_e = end;
             }
             end++;
-            if(end < s.length())
+            if(end < s.size())
                 freqS[s[end]]++;
         }
 
-        return s.substr(ans_s,len);
+        return s.substr(ans_start,len);
+
+
+
+
     }
 };
