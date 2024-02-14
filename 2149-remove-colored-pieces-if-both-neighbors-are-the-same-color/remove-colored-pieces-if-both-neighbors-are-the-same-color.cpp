2ac83347adbbcbb48c89f0bcccac12e5eed5e34e
MONASH CHHETRI <monash.chhetri22@gmail.com>
2024-02-14 11:53:57 +0530

    Time: 36 ms (48.50%) | Memory: 14.2 MB (48.18%) - LeetSync

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -1,57 +1,19 @@
 class Solution {
 public:
-    bool winnerOfGame(string s) {
+    bool winnerOfGame(string colors) {
         
+        int a = 0 , b = 0 ; 
 
-        bool A = false, B = false;
-        int chunkA = 0 , chunkB = 0;
-        int countA = 0 , countB = 0;
-        for(int i=0;i<s.size();i++)
+        for(int i=1;i<colors.size();i++)
         {
-            if(s[i] == 'A' and A)
+            if(colors[i-1] == colors[i+1] and colors[i] == colors[i+1])
             {
-                chunkA++;
-                B = false;
-                chunkB = 0;
+                if(colors[i] == 'A')
+                    a++;
+                else
+                    b++;
             }
-            else if(s[i] == 'A' and !A)
-            {
-                chunkA = 1;
-                A = true;
-                B = false;
-                chunkB = 0;
-            }
-            else if(s[i] == 'B' and B)
-            {
-                chunkB++;
-                A = false;
-                chunkA = 0;
-            }
-            else if(s[i] == 'B' and !B)
-            {
-                chunkB = 1;
-                B = true;
-                A = false;
-                chunkA = 0;
-            }
-
-            if(chunkA >= 3)
-            {
-                countA++;
-            }
-            else if(chunkB >= 3)
-            {
-                countB++;
-            }
-
-
         }
-
-        
-        // cout<<countA<<" "<<countB;
-        if(countA > countB)
-            return true;
-        
-        return false;
+        return a > b ;
     }
 };
