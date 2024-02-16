4b14c7025032a0eab4e8c80549b0f4daf4437a8f
MONASH CHHETRI <monash.chhetri22@gmail.com>
2024-02-16 12:26:26 +0530

    Time: 117 ms (81.69%) | Memory: 66.1 MB (51.33%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     
-    bool static cmp(int x,int y)
-    {
-        return x<y;
-    }
+    
     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
         unordered_map<int,int>M;
 
@@ -12,25 +9,46 @@ public:
             M[x]++;
         
        
-        vector<pair<int,int>>VEC;
+        // vector<pair<int,int>>VEC;
+
+        // for(auto it:M)
+        //     VEC.push_back({it.second , it.first});
+        
+        // sort(VEC.begin(),VEC.end());
+
+        // for(int i=0;i<VEC.size();i++)
+        // {
+        //     if(VEC[i].first < k)
+        //         k -= VEC[i].first;
+        //     else if(VEC[i].first == k)
+        //         return VEC.size()-i-1;
+        //     else
+        //         return VEC.size()-i;
+
+        // }
+        
+        // return -1;
+
+        priority_queue<int,vector<int>,greater<int>> min_heap;
 
         for(auto it:M)
-            VEC.push_back({it.second , it.first});
+            min_heap.push({it.second});
         
-        sort(VEC.begin(),VEC.end());
 
-        for(int i=0;i<VEC.size();i++)
+        while(!min_heap.empty() and k > 0)
         {
-            if(VEC[i].first < k)
-                k -= VEC[i].first;
-            else if(VEC[i].first == k)
-                return VEC.size()-i-1;
-            else
-                return VEC.size()-i;
-
+            int x = min_heap.top();
+            min_heap.pop();
+            k-=x;
+            
+            
         }
-        
-        return -1;
+        if(k == 0)
+                return min_heap.size();
+          
+                return min_heap.size()+1;
+       
+   
 
     }
 };
