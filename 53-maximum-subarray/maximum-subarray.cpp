eb109dc8b1496775d82b9dd24652ce18cd7276ee
MONASH CHHETRI <monash.chhetri22@gmail.com>
2024-03-23 15:36:07 +0530

    Time: 77 ms (61.72%) | Memory: 70.2 MB (67.93%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-
-       int best = INT_MIN;
-       int sum = 0;
-
-       for(int i=0;i < nums.size();i++)
-       {
-            sum = max(nums[i],nums[i]+sum);
-            best = max(best,sum);
-       }
-       return best;
         
+        int ans = nums[0];
+
+        nums[0] = nums[0] < 0 ? 0 : nums[0];
+
+        for(int i=1;i<nums.size();i++)
+        {
+            nums[i] += nums[i-1];
+
+            ans = max(ans,nums[i]);
+
+            if(nums[i] < 0)
+                nums[i] = 0;
+        }
+        return ans;
     }
 };
