a4a3e4803fd71ff6060a3f89790a46a34888471c
MONASH CHHETRI <monash.chhetri22@gmail.com>
2024-04-01 09:23:30 +0530

    Time: 4 ms (47.87%) | Memory: 7.8 MB (20.26%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int ans = 0;
+        
+        int j = s.size()-1;
 
-        int i = s.length()-1;
-
-        int start = false;
-        int count  =0 ;
-        while(i >= 0)
+        while(j >= 0 and s[j] == ' ')
         {
-            if(start == false and s[i] == ' ')
-            {
-                i--;
-                continue;
-            }
-            else if(start == false and s[i] != ' ')
-            {
-                start = true;
-                i--;
-                count++;
-            }
-            else if(start == true and s[i] != ' ')
-            {
-                i--;
-                count++;
-            }
-            else
-                break;
+            j--;
         }
-        return count;
+
+        int ans = 0;
+        while(j >=0 and s[j] != ' ')
+        {
+            ans++;
+            j--;
+        }
+        return ans;
     }
 };
