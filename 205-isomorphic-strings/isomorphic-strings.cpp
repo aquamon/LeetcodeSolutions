12897f492f339bc24b237024cd7c90f5f4c8c056
MONASH CHHETRI <monash.chhetri22@gmail.com>
2024-04-02 10:53:04 +0530

    Time: 7 ms (48.84%) | Memory: 9.4 MB (17.52%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -2,32 +2,35 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         
-        if(s.size() != t.size())
-            return false;
-       unordered_map<char,char>M1;
-       unordered_map<char,char>M2;
-       
+        unordered_map<char,int>M1;
+        unordered_map<char,int>M2;
+        unordered_map<char,char>REP;
+        for(char x : s)
+        {
+            M1[x]++;
+        }
+        for(char x : t)
+        {
+            M2[x]++;
+        }
 
-       for(int i=0;i<t.size();i++)
-       {
-           if(M1.find(s[i]) != M1.end())
-           {
-               if(M1[s[i]] != t[i])
+        if(M1.size() != M2.size())
+        {
+            return false;
+        }
+
+        for(int i=0;i<s.size();i++)
+        {
+            if(M1[s[i]] != M2[t[i]])
                 return false;
-           }
-           else
-           {
-               if(M2.find(t[i]) != M2.end())
-               {
-                   return false;
-               }
-               else
-               {
-                   M1[s[i]] = t[i];
-                   M2[t[i]] = s[i];
-               }
-           }
-       }
-       return true;
+            if(REP.find(s[i]) == REP.end())
+                REP[s[i]] = t[i];
+        }
+
+        for(int i=0;i<s.size();i++)
+        {
+            s[i] = REP[s[i]];
+        }
+        return s==t;
     }
 };
