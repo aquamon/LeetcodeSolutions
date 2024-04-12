d3db09b7e169ab77a0cf2f4e84a26ec649fe4669
MONASH CHHETRI <monash.chhetri22@gmail.com>
2024-04-12 09:55:06 +0530

    Time: 16 ms (15.57%) | Memory: 23 MB (34.69%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,47 +1,27 @@
 class Solution {
 public:
-
-    vector<int> computePrefixMax(vector<int>&arr)
-    {
-        vector<int>ans(arr.size(),INT_MIN);
-        ans[0] = arr[0];
-        for(int i=1;i<arr.size();i++)
-        {
-            ans[i] = max(ans[i-1],arr[i]);
-        }
-
-        return ans;
-    }
-
-    vector<int> computeSuffixMax(vector<int>&arr)
-    {
-        vector<int>ans(arr.size(),INT_MIN);
-        ans[arr.size()-1] = arr[arr.size()-1];
-        for(int i=arr.size()-2;i>=0;i--)
-        {
-            ans[i] = max(ans[i+1],arr[i]);
-        }
-
-        return ans;
-
-    }
     int trap(vector<int>& height) {
         
-        if(height.size() < 3)
-            return 0;
-        vector<int> PrefixMax = computePrefixMax(height);
-        vector<int> SuffixMax = computeSuffixMax(height);
-
+        vector<int>prefixMax(height.size(),-1) , suffixMax(height.size(),-1);
+        prefixMax[0] = height[0];
+        suffixMax[height.size()-1] = height[height.size()-1];
+        for(int i=1;i<height.size();i++)
+        {
+            prefixMax[i] = max(prefixMax[i-1],height[i]);
+                
+        }
+        for(int i=height.size()-2;i>=0;i--)
+        {
+            suffixMax[i] = max(suffixMax[i+1],height[i]);
+        }
+        
         int ans = 0;
+
         for(int i=1;i<height.size()-1;i++)
         {
-            int X = min(PrefixMax[i],SuffixMax[i]);
-            if(X-height[i] > 0)
-                ans += X-height[i];
+            ans += max(min(prefixMax[i],suffixMax[i]) - height[i] , 0);
         }
         return ans;
-
-
 
 
     }
