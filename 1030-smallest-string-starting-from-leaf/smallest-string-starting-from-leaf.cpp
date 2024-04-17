127fc29c23c8ba2ea86c273b82635311ecc58543
MONASH CHHETRI <monash.chhetri22@gmail.com>
2024-04-17 10:10:56 +0530

    Time: 15 ms (13.16%) | Memory: 20.3 MB (78.11%) - LeetSync

---
+++ 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.cpp
@@ -0,0 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    pair<string,string> solve(TreeNode *root)
+    {
+        if(!root)
+            return {"zzzzzz","zzzzzz"};
+        if(!root->left and !root->right)
+        {
+            char x = root->val + 'a';
+            string res = "";
+            res += x;
+            return  {res,res};
+        }
+
+        pair<string,string> Left = solve(root->left);
+        pair<string,string> Right = solve(root->right);
+
+       Left.first += root->val + 'a';
+       Left.second += root->val + 'a';
+
+       Right.first += root->val + 'a';
+       Right.second += root->val + 'a';
+
+       string L = Left.first < Left.second ? Left.first : Left.second;
+       string R = Right.first < Right.second ? Right.first : Right.second;
+
+       return {L,R}; 
+       
+
+        // if(Left < Right)
+        // {
+        //     // char x = root->val + 'a';
+        //     // Left += x;
+        //     return Left;
+        // }
+        // else
+        // {
+        //     // char x = root->val + 'a';
+        //     // Right += x;
+        //     return Right;
+        // }
+
+        // return {Left,Right};
+    }
+    string smallestFromLeaf(TreeNode* root) {
+        
+        // string res = "";
+
+        pair<string,string> res =  solve(root);
+
+        if(res.first < res.second)
+            return res.first;
+        return res.second;
+
+        // return res;
+    }
+};
