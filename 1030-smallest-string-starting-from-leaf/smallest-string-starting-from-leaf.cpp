16e0de9e4de419d213319260f27d993b8c1e6a6b
MONASH CHHETRI <monash.chhetri22@gmail.com>
2024-04-17 10:21:46 +0530

    Time: 8 ms (65.38%) | Memory: 20.4 MB (51.36%) - LeetSync

--- 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.cpp
+++ 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.cpp
@@ -11,41 +11,30 @@
  */
 class Solution {
 public:
-    pair<string,string> solve(TreeNode *root)
+
+    string smallest = "";
+    void dfs(TreeNode* root, string currentString)
     {
         if(!root)
-            return {"zzzzzz","zzzzzz"};
+            return;
+        
+        currentString = char(root->val + 'a') + currentString;
+
         if(!root->left and !root->right)
         {
-            char x = root->val + 'a';
-            string res = "";
-            res += x;
-            return  {res,res};
+            if(smallest == "" or currentString < smallest)
+            {
+                smallest = currentString;
+            }
         }
 
-        pair<string,string> Left = solve(root->left);
-        pair<string,string> Right = solve(root->right);
-
-       Left.first += root->val + 'a';
-       Left.second += root->val + 'a';
-
-       Right.first += root->val + 'a';
-       Right.second += root->val + 'a';
-
-       string L = Left.first < Left.second ? Left.first : Left.second;
-       string R = Right.first < Right.second ? Right.first : Right.second;
-
-       return {L,R}; 
-       
+        dfs(root->left,currentString);
+        dfs(root->right,currentString);
     }
     string smallestFromLeaf(TreeNode* root) {
         
-       
-        pair<string,string> res =  solve(root);
+        dfs(root,"");
 
-        if(res.first < res.second)
-            return res.first;
-        return res.second;
-
+        return smallest;
     }
 };
