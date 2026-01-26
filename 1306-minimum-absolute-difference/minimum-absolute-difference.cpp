adf6506d329d6066797c0a0bc306fe70b0598c57
MONASH CHHETRI <monash.chhetri22@gmail.com>
2026-01-26 16:57:27 +0530

    Time: 74 ms (5.04%) | Memory: 52.5 MB (5.04%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
@@ -1,30 +1,51 @@
 class Solution {
 public:
+     bool static cmp(pair<int,int>&a, pair<int,int>&b)
+    {
+        return a.first < a.second and b.first < b.second;
+    }
+
+    bool static cmp2(vector<vector<int>>&a , vector<vector<int>>&b)
+    {
+        return a[0][0]<a[0][1] and b[0][0] < b[0][1];
+    }
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
         
-        vector<vector<int>>res;
-
         sort(arr.begin(),arr.end());
 
-        int min_diff = INT_MAX;
+        int min_abs_diff = INT_MAX;
         for(int i=1;i<arr.size();i++)
         {
-            int curr = abs(arr[i]-arr[i-1]);
-
-            min_diff = min(min_diff,curr);
+            min_abs_diff = min(min_abs_diff,arr[i]-arr[i-1]);
+        }
+        unordered_map<int,int>HM;
+        for(int i=0;i<arr.size();i++)
+        {
+            HM[arr[i]]++;
         }
 
+        vector<vector<int>> result;
 
-        for(int i=1;i<arr.size();i++)
+        for(int i=0;i<arr.size();i++)
         {
-            int curr = abs(arr[i]-arr[i-1]);
+            int curr = arr[i];
 
-            if(curr == min_diff)
+            if(HM.find(curr-min_abs_diff) != HM.end())
             {
-                res.push_back({arr[i-1],arr[i]});
+                result.push_back({curr,curr-min_abs_diff});
+                HM.erase(curr-min_abs_diff);
             }
         }
 
-        return res;
+        
+        sort(result.begin(),result.end());
+        
+        
+       
+        for(int i=0;i<result.size();i++)
+        {
+            sort(result[i].begin(),result[i].end());
+        }
+        return result;
     }
 };
